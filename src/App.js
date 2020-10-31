6cce14f4aec14c168dd1308796461431cfb50a43
Maycon Queiroz <fmaycon@gmail.com>
2020-10-31 00:22:37 -0300

    challenge on reactjs successfully completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepository] = useState([])
+
+  useEffect(() => {
+
+    api.get('/repositories').then(response => {
+      const repository = response.data;
+
+      setRepository(repository);
+    });
+
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+
+    const repository = await api.post('/repositories', {
+      title: `Gatsby Starter3 ${Date.now()}`,
+      url: "https://github.com/Rocketseat/gatsby-starter-rocket-docs",
+      techs: "[javaScript]"
+    });
+
+    setRepository([...repositories, repository.data]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+    if (repositoryIndex >= 0) {
+
+      await api.delete(`/repositories/${id}`);
+
+      setRepository(repositories.filter(repository => repository.id !== id));
+
+    }
+
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository =>
+          <li key={repository.id}>
+            {repository.title}
+
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+              </button>
+          </li>
+        )}
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
